b604bdfa554509076131df56b8c377b3dad77835
EricD <EricD@NI-24-NTB>
2019-05-20 10:02:57 -0400

    #20 Set up automatic test coverage counting

--- lib/delivery/version.rb
+++ lib/delivery/version.rb
@@ -1,5 +1,5 @@
 module KenticoCloud
   module Delivery
-    VERSION = '1.0.7'.freeze
+    VERSION = '1.0.8'.freeze
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 require 'bundler/setup'
 require 'delivery-sdk-ruby'
 
